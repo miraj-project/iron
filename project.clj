6129eab8948f15692515c0314bd111570c2095d2
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-10 19:01:22 -0600

    initial commit polymer 1.2.3 iron elements

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject miraj.polymer/iron "1.2.3-SNAPSHOT"
+  :description "Polymer Iron Elements"
+  :url "https://github.com/mobileink/polymeraj"
+  :license {:name "BSD License"
+            :url "http://polymer.github.io/LICENSE.txt"})
