5fd79a1e112a893c82972c07b4ce76af42699de2
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-12 18:14:30 -0600

    initial wrapper commit

--- project.clj
+++ project.clj
@@ -2,4 +2,8 @@
   :description "Polymer Iron Elements"
   :url "https://github.com/mobileink/polymer.iron"
   :license {:name "BSD License"
-            :url "http://polymer.github.io/LICENSE.txt"})
+            :url "http://polymer.github.io/LICENSE.txt"}
+  :dependencies [[org.clojure/clojure "1.8.0-RC3"]
+                 [polymer.elements/iron "1.2.3-SNAPSHOT"]
+                 [polymer/core "0.1.0-SNAPSHOT"]
+                 ])
